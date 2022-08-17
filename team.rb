c4eed7ebbc2aa6402e71eb80685e81fc96c777f0
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-08-17 01:37:45 +0000

    first commit

--- team.rb
+++ team.rb
@@ -1,54 +1,27 @@
 class Team
-  attr_accessor :name,:win,:lose,:draw,:score,:name1,:win1,:lose1,:draw1,:score1,:name2,:win2,:lose2,:draw2,:score2,:name3,:win3,:lose3,:draw3,:score3,:name4,:win4,:lose4,:draw4,:score4,:name5,:win5,:lose5,:draw5,:score5
-  def initialize
-    self.name = "Giants"
-    self.win = 67
-    self.lose = 45
-    self.draw = 8
-    self.score = self.win/(self.win + self.lose).to_f
-    self.name1 = "Tigers"
-    self.win1 = 60
-    self.lose1 = 53
-    self.draw1 = 7
-    self.score1 = self.win/(self.win + self.lose).to_f
-    self.name2 = "Dragons"
-    self.win2 = 60
-    self.lose2 = 55
-    self.draw2 = 5
-    self.score2 = self.win/(self.win + self.lose).to_f
-    self.name3 = "BayStars"
-    self.win3 = 56
-    self.lose3 = 58
-    self.draw3 = 6
-    self.score3 = self.win/(self.win + self.lose).to_f
-    self.name4 = "Carp"
-    self.win4 = 52
-    self.lose4 = 56
-    self.draw4 = 12
-    self.score4 = self.win/(self.win + self.lose).to_f
-    self.name5 = "Swallows"
-    self.win5 = 41
-    self.lose5 = 69
-    self.draw5 = 10
-    self.score5 = self.win/(self.win + self.lose).to_f
-  end
-  def test
-    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.score}"
-    puts "#{self.name1}の2020年の成績は#{self.win1}勝#{self.lose1}敗#{self.draw1}分、勝率は#{self.score1}"
-    puts "#{self.name2}の2020年の成績は#{self.win2}勝#{self.lose2}敗#{self.draw2}分、勝率は#{self.score2}"
-    puts "#{self.name3}の2020年の成績は#{self.win3}勝#{self.lose3}敗#{self.draw3}分、勝率は#{self.score3}"
-    puts "#{self.name4}の2020年の成績は#{self.win4}勝#{self.lose4}敗#{self.draw4}分、勝率は#{self.score4}"
-    puts "#{self.name5}の2020年の成績は#{self.win5}勝#{self.lose5}敗#{self.draw5}分、勝率は#{self.score5}"
-  end
+  def initialize(name = "Giants", win = 67, lose = 45, draw = 8)
+    @kyudan = name
+    @kachi = win
+    @make = lose
+    @hikiwake = draw
+    @calc_win_rate = @kachi/(@kachi + @make).to_f
+  end  
+　kyojin = Team.new
+　hanshin = Team.new("Tigers",win = 60,lose = 53, draw = 7)
+　chunichi = Team.new("Dragons", win = 60,lose = 55, draw = 5)
+　yokohama = Team.new("BayStars", win = 56, lose = 58, draw = 6)
+　hiroshima = Team.new("Carp", win = 52, lose = 56, draw = 12)
+　yakuruto = Team.new("Swallows", win = 41, lose = 69, draw = 10)
+
+def show_team_result
+  puts "#{@kyudan}の2020年の成績は#{@kachi}勝#{@make}敗#{@hikiwake}分、勝率は#{@calc_win_rate}です"
 end
-team1 = Team.new
-team1.test
-
-
-
-
-
-
-
+　kyojin.show_team_result
+　hanshin.show_team_result
+　chunichi.show_team_result
+　yokohama.show_team_result
+　hiroshima.show_team_result
+　yakuruto.show_team_result
+end
 
 
