739fe92f544c0f5ffb02b3b15d9b0879695f9b1c
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-08-19 14:04:08 +0000

    first commit

--- team.rb
+++ team.rb
@@ -5,22 +5,25 @@ class Team
        @win = win
        @lose = lose
        @draw = draw
-       @calc_win_rate = @win/(@win + @lose).to_f
+       @show_team_result = show_team_result
      end
-    def show_team_result
-        puts "#{@name}の2020年の成績は#{@win}勝#{@lose}敗#{@draw}分、勝率は#{@calc_win_rate}です"
+    def show_team_result 
+        @show_team_result = @win/(@win + @lose).to_f
+    end
+    def calc_win_rate
+        puts "#{@name}の2020年の成績は#{@win}勝#{@lose}敗#{@draw}分、勝率は#{show_team_result}です"
     end
    end
    kyojin = Team.new("Giants",67,45,8)
    hanshin = Team.new("Tigers",60,53,7)
    chunichi = Team.new("Dragons",60,55,5)
    yokohama = Team.new("BayStars",56,58,6)
-   hiroshima = Team.new("Carp",52,56,12)
-   yakuruto = Team.new("Swallows", 41,69,10)
-   
-   kyojin.show_team_result
-   hanshin.show_team_result
-   chunichi.show_team_result
-   yokohama.show_team_result
-   hiroshima.show_team_result
-   yakuruto.show_team_result
+   hiroshima = Team.new("Carp", 52, 56, 12)
+   yakuruto = Team.new("Swallows", 41, 69,10)
+  
+   kyojin.calc_win_rate
+   hanshin.calc_win_rate
+   chunichi.calc_win_rate
+   yokohama.calc_win_rate
+   hiroshima.calc_win_rate
+   yakuruto.calc_win_rate
