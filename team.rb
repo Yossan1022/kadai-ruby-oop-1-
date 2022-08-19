28b44be2f8e61d2aa5611f96c3677bc10f565850
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-08-19 09:08:31 +0000

    add another.txt

--- team.rb
+++ team.rb
@@ -1,6 +1,6 @@
 class Team
     attr_accessor :name, :win, :lose, :draw
-     def initialize(name = "Giants", win = 67, lose = 45, draw = 8)
+     def initialize(name, win, lose, draw)
        @name = name
        @win = win
        @lose = lose
@@ -11,7 +11,7 @@ class Team
         puts "#{@name}の2020年の成績は#{@win}勝#{@lose}敗#{@draw}分、勝率は#{@calc_win_rate}です"
     end
    end
-   kyojin = Team.new
+   kyojin = Team.new("Giants",win = 67,lose = 45,draw = 8)
    hanshin = Team.new("Tigers",win = 60,lose = 53,draw = 7)
    chunichi = Team.new("Dragons",win = 60,lose = 55,draw = 5)
    yokohama = Team.new("BayStars",win = 56,lose = 58,draw = 6)
